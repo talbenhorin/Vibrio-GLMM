e49b75f890235b5cf99d88a3e1dce4a26cee1792
talbenhorin <36824919+talbenhorin@users.noreply.github.com>
2018-03-21 11:35:14 -0400

    Update MPN_regression.R

--- MPN_regression.R
+++ MPN_regression.R
@@ -1,20 +1,18 @@
-setwd("~/R/MPN")
-rm(list=ls(all=TRUE))
-
+# Install these packages and libraries when you open R or Rstudio 
 install.packages("rjags")
 install.packages("coda")
 install.packages("R2jags")
 install.packages("hdi")
 install.packages("loo")
-
 library(R2jags)
 library(loo)
 
+# Load data - be sure to set you working directory to the location of this file
 dat <- read.csv("ex1.csv", fill = FALSE, header = TRUE) 
+
 m1.dat<-list(c=dat$tlh,v=dat$mass,subj=dat$sample,sal=dat$salinity)
 
-# Fitting MPN data to lognormal-binomial 
-
+# Fitting serial dilution data to lognormal-binomial 
 # The model to be input into JAGS
 cat(
   "model{
@@ -25,10 +23,10 @@ cat(
   mu[i] <- b0[subj[i]]+b1*sal[i]
   }
   for (s in 1:7) {
-  b0[s] ~ dnorm(mu_b0,tau_b0)
+  b0[s] ~ dnorm(mu_logb0,tau_logb0)
   }
-  mu_b0 ~ dnorm(0,0.1)
-  tau_b0 ~ dgamma(0.1,0.1)
+  mu_logb0 ~ dnorm(0,0.1)
+  tau_logb0 ~ dgamma(0.1,0.1)
   tau_all ~ dgamma(0.1,0.1)
   b1 ~ dnorm(0,0.1)
   }",
@@ -36,21 +34,21 @@ cat(
 )
 
 # Initial parameters values to feed into JAGS
-m1.inits <- list(list("b0"=c(0,0,0,0,0,0,0),
-                      "mu_b0"=0,"tau_b0"=0.1,"tau_all"=0.3,"b1"=0),
-                 list("b0"=c(0,0,0,0,0,0,0),
-                      "mu_b0"=0,"tau_b0"=0.2,"tau_all"=0.1,"b1"=0),
-                 list("b0"=c(0,0,0,0,0,0,0),
-                      "mu_b0"=0,"tau_b0"=0.3,"tau_all"=0.2,"b1"=0))
+m1.inits <- list(list("b0"=c(0.1,0,0.1,0,0.1,0,0.1),
+                      "mu_logb0"=0,"tau_logb0"=0.1,"tau_all"=0.3,"b1"=0),
+                 list("b0"=c(0,0.01,0,0.01,0,0.01,0),
+                      "mu_logb0"=0,"tau_logb0"=0.2,"tau_all"=0.1,"b1"=0),
+                 list("b0"=c(0.1,0.01,0.1,0.01,0.1,0.01,0.1),
+                      "mu_logb0"=0,"tau_logb0"=0.3,"tau_all"=0.2,"b1"=0))
 
 # Parameters we ask JAGS to save
-parameters <- c("b1")
+parameters <- c("b1","mu_logb0","tau_logb0","tau_all","b1")
 
 m1 <- jags(data = m1.dat,
            inits = m1.inits,
            parameters.to.save = parameters,
            model.file = "m1.jag",
            n.chains = 3,
-           n.iter = 200000,
-           n.burnin = 5000,
+           n.iter = 10000,
+           n.burnin = 1000,
            n.thin = 3)  
